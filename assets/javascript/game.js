9b1843da7e1b97c1028faeb1073763021b03319f
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-30 16:40:26 -0500

    created the variables for the total score, wins, and losses as 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,10 +11,19 @@ $(document).ready(function() {
 var ran1 = Math.floor(Math.random() * 13 + 1);
 var ran2 = Math.floor(Math.random() * 13 + 1);
 var ran3 = Math.floor(Math.random() * 13 + 1);
-var ran4 = Math.floor(Math.random() * 13 + 1); 
-console.log(ran1)
-console.log(ran2)
-console.log(ran3)
-console.log(ran4)
+var ran4 = Math.floor(Math.random() * 13 + 1);
+console.log(ran1);
+console.log(ran2);
+console.log(ran3);
+console.log(ran4);
 
 //set up variables for the images to be randomly generated between 1-14
+
+//set the variables for the finalTotal, wins, and losses
+
+var total = 0;
+var wins = 0;
+var losses = 0;
+console.log(total)
+console.log(wins)
+console.log(losses)
