8a471422b286fc8585219536bcd3b3425f016b9a
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-30 15:14:39 -0500

    added div classes for the number to guess 'entries', wins, and losses. entered into the js file an array for numbers to guess.

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+var entryNumber = [44, 32, 68, 45, 53, 57]
+//entry number is selected at random
+//entry number is written into entries div
+//number does not change until player either wins or losses
