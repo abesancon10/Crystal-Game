7998cb8555e82d0510092ac00a27c15e93d58617
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-30 16:35:26 -0500

    generated the random numbers for the images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,20 @@
-$( document ).ready(function(){
-    var entry = Math.floor(Math.random() * 43 + 25)
-    $('#entryNumber').text(entry)
-})
-//entry number is selected at random
-//entry number is written into entries div
-//number does not change until player either wins or losses
+$(document).ready(function() {
+  var entry = Math.floor(Math.random() * 43 + 25);
+  $("#entryNumber").text(entry);
+});
+//entry is randomly generated
+//entry should remain between 25 and 68
+//entry appears as text in #entryNumber
+
+//create variables the images that are random numbers
+
+var ran1 = Math.floor(Math.random() * 13 + 1);
+var ran2 = Math.floor(Math.random() * 13 + 1);
+var ran3 = Math.floor(Math.random() * 13 + 1);
+var ran4 = Math.floor(Math.random() * 13 + 1); 
+console.log(ran1)
+console.log(ran2)
+console.log(ran3)
+console.log(ran4)
+
+//set up variables for the images to be randomly generated between 1-14
