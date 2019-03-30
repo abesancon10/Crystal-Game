346f15f174c24c3f8678eab1ab95c028210ae9db
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-30 16:14:32 -0500

    rewrote the HTML file to have the div class and div ids to be more specific to what I beieve I'll be calling back to from the js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,9 @@
-var entryNumber = [44, 32, 68, 45, 53, 57]
 //entry number is selected at random
 //entry number is written into entries div
 //number does not change until player either wins or losses
+$("div.entries").toggleclass(function() {
+    let entryNumber = [44, 32, 68, 45, 53, 57];
+    entryNumber[Math.floor(Math.random() * entryNumber.length)];
+    $("div.entries").text(entryNumber);
+
+})
