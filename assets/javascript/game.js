4718144c40d9a8b5d75bd1bc03b74dca9702c5b5
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-30 16:19:33 -0500

    adjusted the height and width of the images; one image took up the entire page. They have been adjusted to 150px by 150px.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
+var entryNumber = [44, 32, 68, 45, 53, 57];
 //entry number is selected at random
 //entry number is written into entries div
 //number does not change until player either wins or losses
-$("div.entries").toggleclass(function() {
-    let entryNumber = [44, 32, 68, 45, 53, 57];
+$("#entries").text(entryNumber)
     entryNumber[Math.floor(Math.random() * entryNumber.length)];
-    $("div.entries").text(entryNumber);
-
-})
+}
