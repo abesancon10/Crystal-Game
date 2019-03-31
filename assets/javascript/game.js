926fe1da4e91ff5a2f470d800e47b73e4ee20e39
Nuit-Renard <47703634+abesancon10@users.noreply.github.com>
2019-03-30 19:49:06 -0500

    Moved the win and lose function under the #afghan function. Soon found that the entry scope was not in the same scope as the now named 'yes' function. Now the win adds 1 when player matches the entry number if the last click is on the #afghan picture. Also added the losses function as 'opps' and it when total is > entry. Going to place that if/else if conditions under the other picture functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,75 @@
 $(document).ready(function() {
   var entry = Math.floor(Math.random() * 43 + 25);
   $("#entryNumber").text(entry);
-});
-//entry is randomly generated
-//entry should remain between 25 and 68
-//entry appears as text in #entryNumber
 
-//create variables the images that are random numbers
+  //entry is randomly generated
+  //entry should remain between 25 and 68
+  //entry appears as text in #entryNumber
 
-var ran1 = Math.floor(Math.random() * 13 + 1);
-var ran2 = Math.floor(Math.random() * 13 + 1);
-var ran3 = Math.floor(Math.random() * 13 + 1);
-var ran4 = Math.floor(Math.random() * 13 + 1);
-console.log(ran1);
-console.log(ran2);
-console.log(ran3);
-console.log(ran4);
+  //create variables the images that are random numbers
 
-//set up variables for the images to be randomly generated between 1-14
+  var ran1 = Math.floor(Math.random() * 13 + 1);
+  var ran2 = Math.floor(Math.random() * 13 + 1);
+  var ran3 = Math.floor(Math.random() * 13 + 1);
+  var ran4 = Math.floor(Math.random() * 13 + 1);
+  console.log(ran1);
+  console.log(ran2);
+  console.log(ran3);
+  console.log(ran4);
 
-//set the variables for the finalTotal, wins, and losses
+  //set up variables for the images to be randomly generated between 1-14
 
-var total = 0;
-var wins = 0;
-var losses = 0;
-console.log(total);
-console.log(wins);
-console.log(losses);
+  //set the variables for the finalTotal, wins, and losses
 
-//append the the variables wins and losses to the #numberWins and #numberLosses
+  var total = 0;
+  var wins = 0;
+  var losses = 0;
+  console.log(total);
+  console.log(wins);
+  console.log(losses);
 
-$("#numberWins").text(wins);
-$("#numberLosses").text(wins);
-$("#finalTotal").text(total);
+  //append the the variables wins and losses to the #numberWins and #numberLosses
 
-//set up function to add ran1-4 to the total
-//total is added onto the DOM under #finalTotal
-$("#afghan").on("click", function() {
-  total = total + ran1;
-  console.log("the afghan hounds are here: " + ran1);
+  $("#numberWins").text(wins);
+  $("#numberLosses").text(wins);
   $("#finalTotal").text(total);
-});
-$("#saluki").on("click", function() {
-  total = total + ran2;
-  console.log("the saluki's are here: " + ran2);
-  $("#finalTotal").text(total);
-});
-$("#sheltie").on("click", function() {
-  total = total + ran3;
-  console.log("the shelties are here: " + ran3);
-  $("#finalTotal").text(total);
-});
-$("#crested").on("click", function() {
-  total = total + ran4;
-  console.log("the cresteds are here: " + ran4);
-  $("#finalTotal").text(total);
+
+  function yes() {
+    wins++;
+    $("#numberWins").text(wins);
+  }
+  function oops() {
+    losses++;
+    $("#numberLosses").text(losses);
+  }
+  //set up function to add ran1-4 to the total
+  //total is added onto the DOM under #finalTotal
+  $("#afghan").on("click", function() {
+    total = total + ran1;
+    console.log("the afghan hounds are here: " + ran1);
+    $("#finalTotal").text(total);
+    if (total == entry) {
+      yes();
+    } else if (total > entry) {
+      oops();
+    }
+  });
+  $("#saluki").on("click", function() {
+    total = total + ran2;
+    console.log("the saluki's are here: " + ran2);
+    $("#finalTotal").text(total);
+  });
+  $("#sheltie").on("click", function() {
+    total = total + ran3;
+    console.log("the shelties are here: " + ran3);
+    $("#finalTotal").text(total);
+  });
+  $("#crested").on("click", function() {
+    total = total + ran4;
+    console.log("the cresteds are here: " + ran4);
+    $("#finalTotal").text(total);
+  });
 });
 
 //set up the conditions for win
-function yes() {
-  let thePrize = "YOU WIN!";
-  let theKurgan = "YOU LOSE!";
-
-  if (total === entry) {
-    win++;
-    $("#winLose").text(thePrize);
-    $("#numberWins").text(wins);
-  }
-  if (total > entry) {
-    losses++;
-    $("#winLose").text(theKurgan);
-    $("#numberLosses").text(losses);
-  }
-}
+//moved the win lose functions ahead of the picture functions
